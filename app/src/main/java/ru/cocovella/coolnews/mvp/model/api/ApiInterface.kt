ac6479a5d2f9daff230670348beaff60aeb370a5
Cocovella <katherine.zharkova@gmail.com>
2020-04-16 06:05:31 +0300

    added hardcoded RV data, finished with GSON data parsing

--- app/src/main/java/ru/cocovella/coolnews/mvp/model/api/ApiInterface.kt
+++ app/src/main/java/ru/cocovella/coolnews/mvp/model/api/ApiInterface.kt
@@ -9,13 +9,13 @@ import ru.cocovella.coolnews.mvp.model.entity.NewsPublishers
 
 interface ApiInterface {
 
-    @GET("/top-headlines")
+    @GET("top-headlines")
     fun getHeadlines(
         @Query("sources") sourceId: String? = "rbc,tr,lenta,google-news-ru",
         @Query("apiKey") apiKey: String? = "520802e1bb5b4ac1b94aa5927bcf1b00"
     ): Single<List<Headlines>>
 
-    @GET("/sources")
+    @GET("sources")
     fun getPublishers(
         @Query("language") language: String? = "ru",
         @Query("apiKey") apiKey: String? = "520802e1bb5b4ac1b94aa5927bcf1b00"
--- app/src/main/java/ru/cocovella/coolnews/mvp/model/repo/NewsArticlesRepo.kt
+++ app/src/main/java/ru/cocovella/coolnews/mvp/model/repo/NewsArticlesRepo.kt
@@ -9,8 +9,12 @@ import ru.cocovella.coolnews.mvp.model.entity.Source
 class NewsArticlesRepo {
 
     val articles = listOf(
-        Article(Source("abc-news", "BBC News"), "BBC News", "Title 1", "", "",
-            "https://ichef.bbci.co.uk/images/ic/1024x576/p089hx0q.jpg", "2020-04-15T12:30:32Z", ""),
+        Article(Source("rbc", "RBC"), "RBC News", "Трамп заявил о готовности помочь России с аппаратами ИВЛ из-за вируса",
+            "В начале апреля Москва оказала помощь Вашингтону. " +
+                    "Она доставила в Нью-Йорк медицинское оборудование, однако поставка была не безвозмездной. " +
+                    "По данным МИД России, половину оплатил Российский фонд прямых инвестиций, а другую — США",
+            "https://www.rbc.ru/rbcfreenews/5e978f249a79478dba9b8258?from=from_main",
+            "https://s0.rbk.ru/v6_top_pics/media/img/8/38/755869915432388.jpg", "2020-04-15T12:30:32Z", ""),
         Article(Source("bbc-news", "BBC News"), "BBC News", "Title 2", "", "",
             "https://ichef.bbci.co.uk/images/ic/1024x576/p089hx0q.jpg", "2020-04-15T12:30:32Z", ""),
         Article(Source("cbc-news", "BBC News"), "BBC News", "Title 3", "", "",
